d4e361aae68a67a3d223b99139dd17957c3ac39f
mbasavalinga6-design <mbasavalinga6@gmail.com>
2026-01-09 17:08:04 +0530

    Create dictionary_revision.py

---
+++ dictionary_revision.py
@@ -0,0 +1,14 @@
+student = {
+    "Name" : "Basava",
+    "Age" : 20,
+    "Course" : "BCA"
+}
+print(student)
+print(student.keys())
+print(student.values())
+
+student["Name"] = "Basavalinga"
+student["Age"] = 21
+student["Course"] = "BCA Final Year"
+
+print(student)
