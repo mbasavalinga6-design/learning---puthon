d99ad18131bf3f397b412ff302406f649621d4c6
mbasavalinga6-design <mbasavalinga6@gmail.com>
2026-01-12 22:11:32 +0530

    Create condtional_statements.py

---
+++ condtional_statements.py
@@ -0,0 +1,18 @@
+num = int(input("Enter a number: "))
+
+if num > 0:
+    print("Positive number")
+elif num == 0:
+    print("Zero")
+else:
+    print("Negative number")
+
+time = int(input('enter the time:'))
+if time == 8 :
+    print(" it's time for breakfast ")
+elif time == 13 :
+    print( " it's time for lunch ")
+elif time == 20 :
+    print(" it's time for dinner ")
+else:
+    print(" it's not time for meal")
