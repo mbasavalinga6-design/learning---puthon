0ec80ff609214ecfd7f4422bb9cb569c670780a0
mbasavalinga6-design <mbasavalinga6@gmail.com>
2026-01-14 19:24:28 +0530

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,15 @@
+def greet(name):
+    print("Hello", name)
+
+greet("Basava")
+
+def add_num(a,b):
+    return a+b
+total=add_num(10,30)
+print("total sum is:", total)
+
+def add_num(a,b):
+    return a+b
+print(add_num(10,30))
+print(add_num(30,50))
+print(add_num(50,1000))
